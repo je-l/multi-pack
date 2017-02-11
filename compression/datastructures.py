dc1780fa21ea0917ba780c0c4a62aca3a313df9b
je-l <je-l@users.noreply.github.com>
2017-02-11 17:40:29 +0200

    Linked list, hash table, tests

--- compression/datastructures.py
+++ compression/datastructures.py
@@ -4,19 +4,99 @@
 """Data structures."""
 
 
-class Linkedlist:
+class LinkedList:
     """Linked list implementation."""
-
     def __init__(self):
         self.first = None
 
-    def add(self):
+    def add_last(self, key, value):
         """Add new node to this linked list."""
-        pass
+        if self.first is None:
+            self.first = Node(key, value)
+        else:
+            temp_last = self.first
+            while temp_last.next is not None:
+                temp_last = temp_last.next
+            temp_last.next = Node(key, value)
 
-    class Node:
-        """Node in linked list."""
+    def add_first(self, key, value):
+        """Add a new data node to first position in the linked list."""
+        new_node = Node(key, value)
+        new_node.next = self.first
+        self.first = new_node
 
-        def __init__(self, data):
-            self.data = data
-            self.next = None
+    def __str__(self):
+        string = ""
+        node = self.first
+        while node is not None:
+            string += str(node) + ", "
+            node = node.next
+        return string
+
+
+class Node:
+    """Node in linked list."""
+
+    def __init__(self, key, value):
+        self.key = key
+        self.data = value
+        self.next = None
+
+    def __str__(self):
+        return str(self.data)
+
+
+class HashTable:
+    """Hash table with linked list collision handling.
+
+    Hardcoded for 4096 keys. 6151 is the default table size as it's not close
+    to a power of two and it's a prime number.
+    """
+    def __init__(self, size=6151):
+        self.nodes = [None for i in range(size)]
+        self.size = 0
+
+    def __setitem__(self, key, value):
+        """Add new key-value pair to the hash table."""
+        node_hash = self.hash(key)
+        if self.nodes[node_hash] is None:
+            self.nodes[node_hash] = LinkedList()
+            self.nodes[node_hash].add_first(key, value)
+        else:
+            node = self.nodes[node_hash].first
+            while node is not None:
+                if node.key == key:
+                    node.data = value
+                    return
+                node = node.next
+            self.nodes[node_hash].add_last(key, value)
+
+        self.size += 1
+
+    def __getitem__(self, item):
+        """Get value for specified key."""
+        hashed = self.hash(item)
+        if self.nodes[hashed] is not None:
+            node = self.nodes[hashed].first
+            while node is not None:
+                if node.key == item:
+                    return node.data
+
+                node = node.next
+        raise KeyError('key "{}" not in hash table.'.format(item))
+
+    def hash(self, key):
+        """Remainder hashing function as in course material."""
+        hashed = hash(key)
+        return hashed % len(self.nodes)
+
+    def __len__(self):
+        return self.size
+
+    def __str__(self):
+        text = ""
+        for i in range(len(self.nodes)):
+            if self.nodes[i] is None:
+                continue
+            text += str(self.nodes[i]) + "\n"
+        return text
---
+++ compression/test/test_datastructures.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+
+"""Tests for data structures."""
+
+import unittest
+from ..datastructures import Node, LinkedList, HashTable
+
+
+class TestDatastructures(unittest.TestCase):
+
+    def test_node_creation(self):
+        node = Node(1, 2)
+        self.assertEqual(2, node.data)
+
+    def test_node_linking(self):
+        node_o = Node(1, "o")
+        node_k = Node(2, "k")
+        node_o.next = node_k
+
+        self.assertEqual("k", node_o.next.data)
+
+    def test_linked_list_creation(self):
+        linked_list = LinkedList()
+        self.assertIsNone(linked_list.first)
+
+    def test_linked_list_(self):
+        linked_list = LinkedList()
+        self.assertIsNone(linked_list.first)
+
+    def test_linked_list_add_last(self):
+        linked_list = LinkedList()
+        linked_list.add_last("k", 1)
+        linked_list.add_last("o", 2)
+
+        self.assertEqual(1, linked_list.first.data)
+
+    def test_linked_list_small(self):
+        linked_list = LinkedList()
+        for i in range(1, 5):
+            linked_list.add_first(i, i)
+        total = 0
+        node = linked_list.first
+        while node is not None:
+            total += node.data
+            node = node.next
+
+        self.assertEqual(10, total)
+
+    def test_hash_table_length_same_keys(self):
+        hash_table = HashTable()
+        for i in range(3):
+            hash_table["nice"] = 7
+
+        self.assertEqual(1, len(hash_table))
+
+    def test_hash_table_one_key_length(self):
+        hash_table = HashTable()
+        hash_table[2] = "k"
+        self.assertEqual(1, len(hash_table))
+
+    def test_hash_table_full_length(self):
+        hash_table = HashTable()
+        for i in range(4096):
+            hash_table[i] = i
+        self.assertEqual(4096, len(hash_table))
+
+    def test_hash_table_same_key_value_does_change(self):
+        hash_table = HashTable()
+        hash_table["ab"] = 3
+        hash_table["ab"] = 33
+        self.assertEqual(33, hash_table["ab"])
+
+    def test_missing_key_raises_key_error(self):
+        hash_table = HashTable()
+
+        with self.assertRaises(KeyError):
+            k = hash_table["k"]
